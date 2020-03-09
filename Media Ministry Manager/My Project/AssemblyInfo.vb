b1d2055a5d2ce5a877741da428ed9ba83da239e9
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-03-09 11:36:12 -0500

    adding a little security for being on github

--- Media Ministry Manager/My Project/AssemblyInfo.vb
+++ Media Ministry Manager/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.1.28.1")>
-<Assembly: AssemblyFileVersion("1.1.28.1")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
