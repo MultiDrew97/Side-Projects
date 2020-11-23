44d5f1ac3432328f16dacea074095feaa3e40fa6
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-11-23 14:55:32 -0600

    Amending secrets out of commitMade my Database object use parameters in the queries to grant some higher level of security from a sql injection attack to be had on my application

--- Media Ministry Manager/Forms/Inventory/ViewInventory.Designer.vb
+++ Media Ministry Manager/Forms/Inventory/ViewInventory.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frm_ViewInventory
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class frm_ViewInventory
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.dgv_Inventory = New System.Windows.Forms.DataGridView()
